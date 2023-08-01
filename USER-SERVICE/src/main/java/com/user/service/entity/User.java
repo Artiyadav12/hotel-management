4ac2ff5b9e0b337239f59fe079567ae5edb10c85
km.arti <km.arti@moglix.com>
2023-08-01 11:47:16 +0530

    api gateway user service registry aded

---
+++ USER-SERVICE/src/main/java/com/user/service/entity/User.java
@@ -0,0 +1,26 @@
+//package com.user.service.entity;
+//
+//
+//import lombok.Data;
+//import lombok.ToString;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "users")
+//@Data
+//@ToString
+//public class User {
+//    @Id
+//    @Column(name = "id")
+//    private String userId;
+//
+//    @Column(name = "name")
+//    private String name;
+//
+//    @Column(name = "emil")
+//    private String email;
+//}
